a6c5121325e422ca57fdb0a50c4c1344819bdd5f
Alfi Laili Sa'adah <34086559+alfi6706164016@users.noreply.github.com>
2018-03-29 08:58:49 +0700

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -16,10 +16,12 @@ public class Main {
     for(int i = 2; i < n; i++) {
     fib[i] = fib[i-1] + fib[i-2];
     }
-        System.out.println("Output :");
+       System.out.println("jumlah :");
+        long sum=0;
         for (int i = 0; i < n; i++) {
-        System.out.print(fib[i] +" ");
+            sum = fib[i]+sum;
         }
+        System.out.println(sum);
     }
     }
 
